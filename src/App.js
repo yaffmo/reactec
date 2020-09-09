7506eef2358e6b0a19b4c5a0a03c84599856233e
mo_ya <mo_ya@MSI>
2020-09-09 09:57:20 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
+import { HomePage } from './Homepage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HomePage />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
