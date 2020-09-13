dabc89d433efe6a0b8c7a6ea0fc58629a30a7115
YA!福茂 <yaffmo@gmail.com>
2020-09-13 21:50:27 +0800

    co

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import "./App.scss";
-import { HomePage } from "./pages/HomePage/HomePage.component";
+import HomePage from "./pages/HomePage/HomePage.component";
+
 import ShopPage from "./pages/ShopPage/ShopPage.component";
-export const Hatspage = () => {
-  return (
-    <div>
-      <h1>hatspage</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
